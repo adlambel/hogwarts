ac07c0fed39c40c8f712da4cbb932184dedaaa5b
tiboxman <tibogrouet@hotmail.fr>
2020-01-24 00:24:26 +0100

    http as a service

--- client/hogwarts_app/src/service/httpService.js
+++ client/hogwarts_app/src/service/httpService.js
@@ -1,137 +1,140 @@
 const axios = require('axios');
 
-function getProfessors() {
-    return axios.get('/professor')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
+export default class HttpService {
 
-function getStudents() {
-    return axios.get('/student')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
-
-function addProfessor(name, gender) {
-    return axios.post('/professor',{
-        name: name,
-        gender: gender
-      })
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
-
-function addStudent(house, name, gender) {
-    return axios.post('/student',{
-        house: house,
-        name: name,
-        gender: gender
-      })
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
-
-function deleteProfessor(id) {
-    return axios.delete('/professor',{
-        id: id,
-      })
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
-
-function deleteStudent(id) {
-    return axios.delete('/student',{
-        id: id,
-      })
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
-
-function getScores() {
-    return axios.get('/scores')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
-
-function addScore(professor, house, value) {
-    return axios.post('/scores', 
-    {
-        professor: professor,
-        house: house,
-        value: value
+  getProfessors() {
+      return axios.get('/professor')
+    .then(function (response) {
+      // handle success
+      console.log(response);
     })
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+
+  getStudents() {
+      return axios.get('/student')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+
+  addProfessor(name, gender) {
+      return axios.post('/professor',{
+          name: name,
+          gender: gender
+        })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+
+  addStudent(house, name, gender) {
+      return axios.post('/student',{
+          house: house,
+          name: name,
+          gender: gender
+        })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+
+  deleteProfessor(id) {
+      return axios.delete('/professor',{
+          id: id,
+        })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+
+  deleteStudent(id) {
+      return axios.delete('/student',{
+          id: id,
+        })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+
+  getScores() {
+      return axios.get('/scores')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+
+  addScore(professor, house, value) {
+      return axios.post('/scores', 
+      {
+          professor: professor,
+          house: house,
+          value: value
+      })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
 }
