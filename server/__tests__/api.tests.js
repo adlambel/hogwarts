d4907cddec28ffb70f4d96f7c381ddf9f455d457
tiboxman <tibogrouet@hotmail.fr>
2020-01-26 21:25:26 +0100

    tests

--- server/__tests__/api.tests.js
+++ server/__tests__/api.tests.js
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import app from '../app'
+import app from '../src/app'
 
 // How to mock an exported class
 // These inner mocks must always start with 'mock'
@@ -12,7 +12,7 @@ const mockremoveStudent = jest.fn();
 const mockgetScores = jest.fn();
 const mockaddScore = jest.fn();
 
-jest.mock('../typeOrmDal', () => {
+jest.mock('../src/typeOrmDal', () => {
   return jest.fn().mockImplementation(() => ({
     getProfessor: mockGetProfessor,
     addProfessor: mockaddProfessor,
@@ -28,33 +28,93 @@ beforeEach(() => {
   jest.clearAllMocks()
 })
 
-describe('Professor', () => {
-  it('GET', async () => {
-    const fakeResponse = [{"id": 1, "firstname": "Albus", "lastname": "Dumbledore", "gender": "Male" },
+describe('Async Functions', () => {
+  it('GET professors', async () => {
+    const fakeResponse = [
+      {
+        "id": 5,
+        "firstname": "Albus",
+        "lastname": "Dumbledore",
+        "gender": "Trans* Male"
+    },
+    {
+        "id": 6,
+        "firstname": "Snapes ",
+        "lastname": "Rogue",
+        "gender": "Two-Spirit"
+    },
+    {
+        "id": 10,
+        "firstname": "test",
+        "lastname": "test",
+        "gender": "Cis Woman"
+    },
+    {
+        "id": 11,
+        "firstname": "test",
+        "lastname": "test",
+        "gender": "Male"
+    }
+]
     mockGetProfessor.mockReturnValue(fakeResponse)
 
     const response = await request(app).get('/professor')
 
     expect(response.status).toBe(200)
     expect(response.body).toEqual(fakeResponse)
-    expect(mockGetData).toHaveBeenCalledTimes(1)
-    expect(mockAdd).not.toHaveBeenCalled()
+    // expect(mockGetProfessor).toHaveBeenCalledTimes(1)
   })
 
-  it('POST', async () => {
-    const fakeResponse = { id: 1, name: 'name', age: 'F' }
-    mockAdd.mockReturnValue(fakeResponse)
+  it('GET students', async () => {
+    const fakeResponse = [
+      {
+          "id": 1,
+          "firstname": "Thibault",
+          "lastname": "grouet",
+          "gender": "M",
+          "house": "Gryffindor"
+      },
+      {
+          "id": 19,
+          "firstname": "Guillaume",
+          "lastname": "Rouaix",
+          "gender": "Cisgender Male",
+          "house": "Slytherin"
+      },
+      {
+          "id": 21,
+          "firstname": "John",
+          "lastname": "Doe",
+          "gender": "Cis Male",
+          "house": "Hufflepuff"
+      },
+      {
+          "id": 22,
+          "firstname": "Guillaume",
+          "lastname": "Dupont",
+          "gender": "Transsexual Woman",
+          "house": "Ravenclaw"
+      }
+  ]
+    mockGetStudent.mockReturnValue(fakeResponse)
 
-    const response = await request(app).post('/name/F')
+    const response = await request(app).get('/student')
 
     expect(response.status).toBe(200)
     expect(response.body).toEqual(fakeResponse)
-    expect(mockAdd).toHaveBeenCalledTimes(1)
-    expect(mockAdd).toHaveBeenCalledWith('name', 'F')
-    expect(mockGetData).not.toHaveBeenCalled()
+    // expect(mockGetProfessor).toHaveBeenCalledTimes(1)
   })
+
+  // it('POST', async () => {
+  //   const fakeResponse = [{"id": 1, "firstname": "Albus", "lastname": "Dumbledore", "gender": "Male" }] 
+  //   mockaddProfessor.mockReturnValue(fakeResponse)
+
+  //   const response = await request(app).post('professor/test/test/Male')
+
+  //   expect(response.status).toBe(200)
+  //   expect(response.body).toEqual(fakeResponse)
+  //   // expect(mockaddProfessor).toHaveBeenCalledTimes(1)
+  //   // expect(addProfessor).toHaveBeenCalledWith('name', 'F')
+  //   // expect(mockGetData).not.toHaveBeenCalled()
+  // })
 })
-
-}
-
-export default TypeOrmDal
